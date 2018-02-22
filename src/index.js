18affd1921b63beff5384fad4f035a970bd6bba2
mhutchison90 <michaelhutchison90@gmail.com>
2018-02-21 23:40:30 -0700

    changed service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { unregister } from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
@@ -10,4 +10,4 @@ ReactDOM.render(
     <Router>
         <App />
     </Router>, document.getElementById('root'));
-unregister();
+registerServiceWorker();
